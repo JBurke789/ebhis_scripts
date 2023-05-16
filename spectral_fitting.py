bbcc0e8f8e0ad78b9aa10f0b4aa7d2c9388f1d1f
JBurke789 <burkej13@tcd.ie>
2023-05-16 17:13:36 +0200

    Fits peak with a single Gaussain. Outputs parameters of the fitting. Integrates the spectrum to find total flux.

--- spectral_fitting.py
+++ spectral_fitting.py
@@ -1,28 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
-import warnings
-from astropy import units as u
-from astropy.modeling import models
-import specutils
-
-
-
+from scipy import optimize
+from scipy import integrate
 
 #load spectral data from file and assign to arrays
 array = np.load('spectrum.npy')
 velo = array[0]
-temp = array[1]
+flux = array[1]
 
 #plot quick spectrum
 fig1,ax1 = plt.subplots()
-ax1.plot(velo,temp)
-ax1.set_xlabel('velocity [km/s]')
-ax1.set_ylabel('brightness temp [K]')
+ax1.plot(velo,flux)
+ax1.set_xlabel('Velocity [km/s]')
+ax1.set_ylabel('Specific flux [Jy/BA]')
+
+#fitting with a single gaussian
+def gauss(p,x):#define gaussian function
+    return p[0]*np.exp( -(x-p[1])**2 /(2*p[2]**2)) +p[3]
+def resids(p):#define vector of residuals
+    return gauss(p,velo) - flux
+#initial guess of parameters
+#p[0]= amplitude- value of peak
+#p[1]= mean- peak center velocity
+#p[2]= standard deviation, slightly above half of height 
+#p[3]= offset
+p_init = [610., 160., 60., 0.]
+#run optimization
+out = sp.optimize.least_squares(resids,p_init)
+print(out.x)#print out parameters found by the fitting
+ax1.plot(velo,gauss(out.x,velo))#plot fit
 plt.show()
 
-#baseline correction
-spectrum = specutils.spectra.Spectrum1D(flux=temp*u.Jy,spectral_axis=velo*u.um)
-with warnings.catch_warnings():
-    warnings.simplefilter('ignore')
-    gl_fit = fit_generic_continuum(spectrum)
+#integrate to get total flux
+tot_flux = sp.integrate.simps(flux,velo)
+print(tot_flux)
+
+
+
+
