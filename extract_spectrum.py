0c4100ee0dbe7b448b2bcfd11d20c57b5b3c879a
JBurke789 <burkej13@tcd.ie>
2023-05-19 16:00:33 +0200

    Extracts spectrum from box around galaxy and saves as .npy file

---
+++ extract_spectrum.py
@@ -0,0 +1,42 @@
+import scipy as sp
+import numpy as np
+import os
+import copy
+
+plt.close()
+#define input galaxy details.
+im_name='raw_image.im'
+xbl= 130 #box around galaxy
+ybl= 130
+xtr= 170
+ytr= 170
+
+low_chan=83
+high_chan= 106
+dB = 'G' 
+blc=[xbl,ybl, 50]
+trc=[xtr,ytr,233]
+
+#extract spectrum from datacube
+ia.open(im_name)
+flux = ia.getchunk(blc,trc)
+ia.close()
+sumspec =np.sum(np.sum(flux,axis=0),axis=0) #collapse into 1D array
+
+#extract velocities for x axis
+ia.open(im_name)
+csys=ia.coordsys()
+x=np.array(np.arange(0,len(sumspec),1.)) # need floating point array
+freqs = copy.deepcopy(x)
+velo=copy.deepcopy(x)
+blctemp=copy.deepcopy(blc)
+for a in range(0,len(x)):
+  blctemp[2]=x[a]+blc[2]
+  w=ia.toworld(blctemp,'n')
+  freq=w['numeric'][2] # gives frequencies
+  v=csys.frequencytovelocity(value=freq,doppler='radio',velunit='km/s')
+  velo[a]=float(v)
+
+#save spectrum to numpy file [velocity,flux/Beam Area]
+output = np.stack((velo,sumspec),axis=0)
+np.save('spectrum.npy',output)
