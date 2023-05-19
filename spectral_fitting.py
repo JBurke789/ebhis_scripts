d2931bc49deb2635b6b5727b0eaf86b737270579
JBurke789 <burkej13@tcd.ie>
2023-05-19 16:02:56 +0200

    minor changes. Wrote plan for future work as comments.

--- spectral_fitting.py
+++ spectral_fitting.py
@@ -1,17 +1,23 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy import optimize
 from scipy import integrate
 
+"""
+npy file now contains temp and velocity. need to change to flux using conversion.
+"""
+
 #load spectral data from file and assign to arrays
 array = np.load('spectrum.npy')
 velo = array[0]
-flux = array[1]
+temp = array[1]
 
 #plot quick spectrum
 fig1,ax1 = plt.subplots()
-ax1.plot(velo,flux)
+ax1.plot(velo,temp)
 ax1.set_xlabel('Velocity [km/s]')
 ax1.set_ylabel('Specific flux [Jy/BA]')
 
@@ -35,6 +41,17 @@ plt.show()
 #integrate to get total flux
 tot_flux = sp.integrate.simps(flux,velo)
 print(tot_flux)
+
+H0 = 67.7 #planck collab 2018
+r = lambda vel: vel/H0 #give vel in km/s returns distance in Mpc
+
+def mass(vel,flux_int):#returns HI mass in units of solar masses
+    return 2.36e5*(r(vel)**2)*flux_int
+print(r(out.x[1]))
+mass_HI =  "{:.2e}".format(mass(out.x[1],tot_flux))
+print('HI mass: ')
+print(mass_HI)
+
 
 
 
