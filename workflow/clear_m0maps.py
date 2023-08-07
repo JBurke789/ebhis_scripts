fa7f42d6faf4805a4a335598fe5c0405eafe9350
JBurke789 <burkej13@tcd.ie>
2023-08-07 12:51:02 +0200

    removes all moment zero maps already created

--- workflow/clear_m0maps.py
+++ workflow/clear_m0maps.py
@@ -1,6 +1,6 @@
 import csv 
 import os
-
+#removes all mom 0 maps already generated
 with open('/users/jburke/Desktop/gal_candidates_big.csv','r') as f:
     reader = csv.reader(f)
     for row in reader:#looks at each galaxy
