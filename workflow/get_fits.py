82c0f9e4cd1f6965ba2aa45b0b9174d70ac1f605
JBurke789 <burkej13@tcd.ie>
2023-08-07 17:39:47 +0200

    requests data cubes for all galaxy candidates.

--- workflow/get_fits.py
+++ workflow/get_fits.py
@@ -1,16 +1,21 @@
 import os
 import csv
-"""From candidate list, runs command to make datacubes from EBHIS, saves each to its own directory. Needs enviroment to be open already"""
 
-file_path = '/users/jburke/Desktop/gal_candidates_big.csv'
+"""
+Run after gal_sort.py . 
 
+Runs through the CSV files generated of galaxy candidates. Runs command to make datacubes from EBHIS. 
+Saves each datacube to its own directory.
+Needs enviroment already open.
+
+"""
 def data_request(name, ra ,dec):
     #make directory for galaxy
     if ' ' in name:
         name = name.replace(' ','')
 
     try:
-        os.mkdir(name)
+        os.mkdir(name)#makes directory for galaxy
     except FileExistsError:
         print("Directory for '{name}' already exists.")
 
@@ -22,12 +27,35 @@ def data_request(name, ra ,dec):
     command = 'python /vol/ebhis2/data1/bwinkel/software/hpxtools/hpxgrid4.py -ds EBHIS -s E -l ' + ra_vals + dec_vals + filename
     os.system(command)    
 
-with open(file_path,'r') as f:
-    reader = csv.reader(f)
+autogen_file_path = '/users/jburke/ebhis_scripts/workflow_results/auto_analyse.csv'
+mw_overlap_file_path = '/users/jburke/ebhis_scripts/workflow_results/MW_overlap.csv'
+no_w50_file_path ='/users/jburke/ebhis_scripts/workflow_results/no_linewidth.csv'
 
+
+
+with open(autogen_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
     for row in reader:
         name = row[0]
         ra= row[1]
         dec = row[2]
+        data_request(name,ra,dec)
 
+with open(mw_overlap_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        name = row[0]
+        ra= row[1]
+        dec = row[2]
+        data_request(name,ra,dec)
+
+with open(no_w50_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        name = row[0]
+        ra= row[1]
+        dec = row[2]
         data_request(name,ra,dec)
