4614d0fd299bf791bad17fb81e3f73e4236ef8ff
JBurke789 <burkej13@tcd.ie>
2023-08-08 17:55:58 +0200

    working for full list

--- workflow/get_fits.py
+++ workflow/get_fits.py
@@ -10,6 +10,9 @@ Needs enviroment already open.
 
 """
 def data_request(name, ra ,dec):
+    print('...')
+    print('galaxy  --->  '+name)
+    print('...')
     #make directory for galaxy
     if ' ' in name:
         name = name.replace(' ','')
@@ -17,7 +20,7 @@ def data_request(name, ra ,dec):
     try:
         os.mkdir(name)#makes directory for galaxy
     except FileExistsError:
-        print("Directory for '{name}' already exists.")
+        print("Directory for "+ name + " already exists.")
 
     #make data request
     ra_vals = str(float(ra) - 3) + ' ' + str(float(ra) +3)
@@ -27,34 +30,74 @@ def data_request(name, ra ,dec):
     command = 'python /vol/ebhis2/data1/bwinkel/software/hpxtools/hpxgrid4.py -ds EBHIS -s E -l ' + ra_vals + dec_vals + filename
     os.system(command)    
 
-autogen_file_path = '/users/jburke/ebhis_scripts/workflow_results/auto_analyse.csv'
-mw_overlap_file_path = '/users/jburke/ebhis_scripts/workflow_results/MW_overlap.csv'
-no_w50_file_path ='/users/jburke/ebhis_scripts/workflow_results/no_linewidth.csv'
+autogen_file_path = '/users/jburke/ebhis_scripts/full_workflow_results/auto_analyse.csv'
+mw_overlap_file_path = '/users/jburke/ebhis_scripts/full_workflow_results/MW_overlap.csv'
+no_w50_file_path ='/users/jburke/ebhis_scripts/full_workflow_results/no_linewidth.csv'
 
 
-
+print('...')
+print('auto_analyse.csv galaxies')
+print('...')
 with open(autogen_file_path,'r') as f:
     reader = csv.reader(f)
     header = next(reader)
+    #length = str(len(list(reader)))
+    i=0
     for row in reader:
+        i+=1
+    length = i
+with open(autogen_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    j=1
+    for row in reader:
+        print(str(j)+ ' of '+ str(length))
+        j+=1
         name = row[0]
         ra= row[1]
         dec = row[2]
         data_request(name,ra,dec)
-
+print('...')
+print('MW_overlap.csv galaxies')
+print('...')
 with open(mw_overlap_file_path,'r') as f:
     reader = csv.reader(f)
     header = next(reader)
+    #length = str(len(list(reader)))
+    i=0
     for row in reader:
+        i+=1
+    length = i
+with open(mw_overlap_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    j=1
+    for row in reader:
+        print(str(j)+ ' of '+ str(length))
+        j+=1
         name = row[0]
         ra= row[1]
         dec = row[2]
         data_request(name,ra,dec)
 
+print('...')
+print('no_linewidth.csvgalaxies')
+print('...')
 with open(no_w50_file_path,'r') as f:
     reader = csv.reader(f)
     header = next(reader)
+    #length = str(len(list(reader)))
+    i=0
     for row in reader:
+        i+=1
+    length = i
+with open(no_w50_file_path,'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    j=1
+    for row in reader:
+        print(str(j)+ ' of '+ str(length))
+        j+=1
         name = row[0]
         ra= row[1]
         dec = row[2]
