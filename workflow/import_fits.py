e741be4871ad91e21204c34efb9dfef083e83d08
JBurke789 <burkej13@tcd.ie>
2023-08-08 17:56:49 +0200

    runs through csv files of galaxies with datacubes downloaded and imports the fits files to CASA format

--- workflow/import_fits.py
+++ workflow/import_fits.py
@@ -8,16 +8,45 @@ def import_fit(name):
     importfits(fitsimage=datacube_path,
            imagename=rawim_path)
     
-#runs through list of galaxy candidates and imports datacube if it has not already been done
-with open('/users/jburke/Desktop/gal_candidates_big.csv','r') as f:
+#runs through lists of galaxies with datacubes and imports datacube if it has not already been done
+with open('/users/jburke/ebhis_scripts/workflow_results/auto_analyse.csv','r') as f:
     reader = csv.reader(f)
+    header = next(reader)
     for row in reader:#looks at each galaxy
         name= row[0]
         if ' ' in name:
             name = name.replace(' ','')
         dir = name+'/raw_image.im'
         if os.path.isdir(dir):
-            print(name+'fits file already imported')
+            print(name+' fits file already imported')
+        else:
+            import_fit(name)
+            print(name,' fits file imported')
+
+with open('/users/jburke/ebhis_scripts/workflow_results/MW_overlap.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:#looks at each galaxy
+        name= row[0]
+        if ' ' in name:
+            name = name.replace(' ','')
+        dir = name+'/raw_image.im'
+        if os.path.isdir(dir):
+            print(name+' fits file already imported')
+        else:
+            import_fit(name)
+            print(name,' fits file imported')
+
+with open('/users/jburke/ebhis_scripts/workflow_results/no_linewidth.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:#looks at each galaxy
+        name= row[0]
+        if ' ' in name:
+            name = name.replace(' ','')
+        dir = name+'/raw_image.im'
+        if os.path.isdir(dir):
+            print(name+' fits file already imported')
         else:
             import_fit(name)
             print(name,' fits file imported')
