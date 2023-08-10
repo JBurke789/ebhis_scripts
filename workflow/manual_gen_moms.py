208c0678ef37939bcd778e554b270335e754d2a0
JBurke789 <burkej13@tcd.ie>
2023-08-10 17:36:28 +0200

    Looks at destination csvs to see if a galaxy has already been analysed. Either generates m0 map or send to MW overlap csv

--- workflow/manual_gen_moms.py
+++ workflow/manual_gen_moms.py
@@ -14,26 +14,68 @@ def mom0_map(name,low_vel,high_vel):
               moments=[0],
               chans=channels,
               outfile=out)
-#creates empty csv to store galaxies that cant make moment 0 map
-with open('/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv','w') as f:
+    
+#looks for csv file to store gals that can't be analysed, makes one if not there
+csvpath = '/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv'
+if not os.path.exists(csvpath):
+    with open('/users/jburke/Desktop/test_gal_list.csv','r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        with open('/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv','w') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(header)
+else:
     pass
+
+#makes a list of all galaxy names that have been analysed
+galaxies_analysed =[]
+with open('/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv','r') as csv_file:
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    for row in reader:
+        name = row[0]
+        galaxies_analysed.append(name)
+with open('/users/jburke/ebhis_scripts/workflow_results/MW_overlap.csv','r') as csv_file:
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    for row in reader:
+        name = row[0]
+        galaxies_analysed.append(name)
+with open('/users/jburke/ebhis_scripts/workflow_results/gals_with_m0maps.csv','r') as csv_file:
+    reader = csv.reader(csv_file)
+    header = next(reader)
+    for row in reader:
+        name = row[0]
+        galaxies_analysed.append(name)
+
 
 with open('/users/jburke/ebhis_scripts/workflow_results/no_linewidth.csv','r') as f:
     reader = csv.reader(f)
-    for row in reader:#looks at each galaxy
-        raw_im_path = row[0]+'/raw_image.im'
-        imview(raw_im_path)
-        cont = input('Can a Mom 0 map be generated?: (y/n)')
-        if cont == 'n':
-            #add to cant make mom0 list
-            with open('/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv','a') as file:
-                write= csv.writer(file)
-                write.writerow(row)
-        else :
-            low_chan = int(input('Galaxy low channel number:'))
-            high_chan = int(input('Galaxy high channel number:'))
-            mom0_map(row[0],low_chan,high_chan)
-            #adds to csv list of galaxies with mom0 maps
-            with open('/users/jburke/ebhis_scripts/workflow_results/gals_with_m0maps.csv','a')as file:
-                write= csv.writer(file)
-                write.writerow(row)
+    header = next(reader)
+    for row in reader:
+        if row[0] in galaxies_analysed:#skip if already analysed
+            print(str(row[0])+' already analysed')
+        else:
+            print('Radial vel-- ' + str(row[4]))
+            raw_im_path = row[0]+'/raw_image.im'
+            imview(raw_im_path)
+            visible = input('Is galaxy visible?: (y/n)')
+            if visible =='n':          #add to cant make mom0 list
+                with open('/users/jburke/ebhis_scripts/workflow_results/cant_make_m0map.csv','a') as file:
+                    write= csv.writer(file)
+                    write.writerow(row)  
+            elif visible =='y':
+                MW_overlap = input('Is there MW overlap?: (y/n)')
+                if MW_overlap == 'y':
+                    #send to MW overlap csv
+                    with open('/users/jburke/ebhis_scripts/workflow_results/MW_overlap.csv','a')as file:
+                        write= csv.writer(file)
+                        write.writerow(row)
+                elif MW_overlap=='n':#generates m0 map
+                    low_chan = int(input('Galaxy low channel number:'))
+                    high_chan = int(input('Galaxy high channel number:'))
+                    mom0_map(row[0],low_chan,high_chan)
+                    #adds to csv list of galaxies with mom0 maps
+                    with open('/users/jburke/ebhis_scripts/workflow_results/gals_with_m0maps.csv','a')as file:
+                        write= csv.writer(file)
+                        write.writerow(row)
