75b3223fd0692b5e7356fb1235765fc612005724
JBurke789 <burkej13@tcd.ie>
2023-08-30 15:30:55 +0200

    Automatically imports fits file and generates moment 0 map from galaxy velocities where possible.

--- group_gal_workflow/gen_mom_map.py
+++ group_gal_workflow/gen_mom_map.py
@@ -1,20 +1,62 @@
 import csv
 import numpy as np
 import os
+"""
+imports fits to CASA format. Calculates the velocity range to make moment maps in and makes it if it can.
+Needs to be run in CASA
 
-imview('raw_image.im')
-low_chan= input('Low Channel: ')
-high_chan=input('High Channel: ')
-#function to make moment 0 map, given the frame numbers of the galaxy
-def mom0_map(low_vel,high_vel):
-    #assign channels of galaxy
-    channels=str(low_vel)+'~'+str(high_vel)
-    #create moment 0 map with no filter
-    raw_im ='raw_image.im' 
-    out = 'moment0.im'
-    immoments(imagename=raw_im,
+source /vol/software/software/astro/casa/initcasa.sh
+"""
+
+importfits(fitsimage='data_cube.fits',
+           imagename='raw_image.im' )
+
+
+gal_info = np.load('group_info.npy')
+
+min_vels=[]
+max_vels=[]
+for i in gal_info:
+    rad_vel= float(i[4])
+    if i[6]=='':
+        w50=0
+    else:
+        w50=float(i[6])
+    min_vels.append(rad_vel-w50)
+    max_vels.append(rad_vel+w50)
+
+low_lim  =min(min_vels)-10
+high_lim =max(max_vels)+10
+vels = np.linspace(-606.648,609.427,945)
+l_ind = np.argmin(np.abs(np.array(vels)-low_lim))
+h_ind = np.argmin(np.abs(np.array(vels)-high_lim))
+channels= str(l_ind)+'~'+str(h_ind)
+
+
+if high_lim<=609.425 and low_lim>=50:
+    print(str(low_lim)+'km/s -> '+str(high_lim)+'km/s')
+    immoments(imagename='raw_image.im',
               moments=[0],
               chans=channels,
-              outfile=out)
-    
-mom0_map(low_chan,high_chan)
+              outfile='moment0.im')
+    print('Moment 0 map created.')
+elif low_lim>=-606.648 and high_lim<=-50:
+    print(str(low_lim)+'km/s -> '+str(high_lim)+'km/s')
+    immoments(imagename='raw_image.im',
+              moments=[0],
+              chans=channels,
+              outfile='moment0.im')
+    print('Moment 0 map created.')
+else:
+    print(str(low_lim)+'km/s -> '+str(high_lim)+'km/s')
+    imview('raw_image.im')
+    valid = input('Can moment map be generated? (y/n): ')
+    if valid =='y':
+        low_chan= input('Low Channel: ')
+        high_chan=input('High Channel: ')
+        channels=str(low_chan)+'~'+str(high_chan)
+        immoments(imagename='raw_image.im',
+                  moments=[0],
+                  chans=channels,
+                  outfile='moment0.im')
+
