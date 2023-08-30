4ef8796f75cdee3c46a78229a1e918d975f938c9
JBurke789 <burkej13@tcd.ie>
2023-08-30 16:49:08 +0200

    Results are overwritten if already analysed and appended if not. csv is also organised by group ID

--- group_gal_workflow/mom_map_analysis.py
+++ group_gal_workflow/mom_map_analysis.py
@@ -2,6 +2,14 @@ import csv
 import os
 import numpy as np
 
+gal_results=[]
+with open('/users/jburke/ebhis_scripts/group_gal_results/group_results.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        gal_results.append(row)
+
+print(gal_results)
 
 imview('moment0.im')
 group = input('group ID: ')
@@ -10,10 +18,7 @@ in_sum = float(input('Inner sum: '))
 in_npix = float(input('Inner npix: '))
 out_sum = float(input('Outer sum: ')) 
 out_npix = float(input('Outer npix: '))
-            #out_rms =  float(input('Outer rms: '))
-            #bg_rms = np.sqrt((out_npix/(out_npix-in_npix))*(out_rms**2 - (in_npix/(out_npix))*in_rms**2))
 bg_rms = 0.09# rms=90mK from ebhis calibration
-            #calculates flux from values in annuli
 bg_flux = out_sum-in_sum
 bg_npix = out_npix-in_npix
 bg_per_pix = bg_flux/bg_npix
@@ -25,11 +30,26 @@ frac_uncert = uncert/clean_flux
 norm_uncert = frac_uncert*norm_flux_jy 
 print('norm flux = '+ str(norm_flux_jy)+' p/m '+ str(norm_uncert))
 
+
 save = input('Save results? y/n: ')
 if save == 'y':
-    with open('/users/jburke/ebhis_scripts/group_gal_results/group_results.csv','a') as f:
-        lines =[group,
-                str(norm_flux_jy),
-                str(norm_uncert),
-                '\n']
-        f.write(','.join(lines))   
+    #run through gals already analysed and replace values
+    groups_analysed=[]
+    for i in gal_results:
+        groups_analysed.append(i[0])
+        if i[0]==group:
+            i[1]=str(norm_flux_jy)
+            i[2]=str(norm_uncert)
+    #append gal vals if not already analysed
+    if group not in groups_analysed:
+        new_row = [group,str(norm_flux_jy),str(norm_uncert)]
+        gal_results.append(new_row)
+
+    sorted_gal_results= sorted(gal_results,key = lambda x: float(x[0]))
+    with open('/users/jburke/ebhis_scripts/group_gal_results/group_results.csv','w') as f:
+        csv_writer = csv.writer(f)
+        header=['Group','flux [Jy km/s /BA]','uncert']
+        csv_writer.writerow(header)
+        csv_writer.writerows(sorted_gal_results)
+
+
