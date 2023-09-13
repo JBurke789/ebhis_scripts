1398ecee9fdd2dc5ac08a4910bdc386002be1454
JBurke789 <burkej13@tcd.ie>
2023-09-13 14:26:04 +0200

    .

---
+++ workflow/region_make.py
@@ -0,0 +1,14 @@
+import csv
+
+
+
+
+with open('/users/jburke/ebhis_scripts/workflow_results/need_manual_analysis.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        file1 =  row[0]+'/location.crtf'
+        with open(file1,'w') as file:
+            lines = ['#CRTFv0 CASA Region Text Format version 0 \n',
+                      'ellipse [[',row[1],'deg,',row[2],'deg], [800arcsec, 800arcsec], 0.00000000deg],label="',row[0],'"']
+            file.write(''.join(lines))
