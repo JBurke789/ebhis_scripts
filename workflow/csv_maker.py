60a1c755c9929257feff6802596fdf64c2d50f8a
JBurke789 <burkej13@tcd.ie>
2023-10-09 17:00:56 +0200

    latest updates

--- workflow/csv_maker.py
+++ workflow/csv_maker.py
@@ -3,8 +3,6 @@ import csv
 with open('/users/jburke/ebhis_scripts/w50_stuff/overlap_spectra.csv','r') as file:
     reader = csv.reader(file)
     header = next(reader)
-    with open('/users/jburke/ebhis_scripts/w50_stuff/rv_w50_vals.csv','w') as f:
+    with open('/users/jburke/ebhis_scripts/group_gal_results/single_group_gals.csv','w') as f:
         writer = csv.writer(f)
-        adds = ['RV [km/s]','uncert','w50 [km/s]','uncert']
-        new_header = header+adds
-        writer.writerow(new_header)
+        writer.writerow(header)
