99e645cbd532438c07679cf8883118945831d1b9
JBurke789 <burkej13@tcd.ie>
2023-10-11 16:59:46 +0200

    now does outside in w50 method and does full spectra, not minus bg

--- w50_stuff/get_values.py
+++ w50_stuff/get_values.py
@@ -11,7 +11,6 @@ def load_array(name,spec):
 
 def get_spectra(name):
     nf= load_array(name,'/spectrum.npy')
-    
     h1 = load_array(name,'/hanning_spectrum1.npy')
     h2 = load_array(name,'/hanning_spectrum2.npy')
     h3 = load_array(name,'/hanning_spectrum3.npy')
@@ -20,13 +19,11 @@ def get_spectra(name):
 
 def plot_all_spectra(name,nf,h1,h2,h3,h4,rv):
     fig1,ax1 = plt.subplots()
-    #y_hanning = cleaned_gal*np.hanning(len(vel))
-    ax1.plot(nf[0],nf[1]-nf[2],label='no filter')
-    ax1.plot(h4[0],h4[2],label='bg')
-    ax1.plot(h1[0],h1[1]-h1[2],label='hanning 1')
-    ax1.plot(h2[0],h2[1]-h2[2],label='hanning 2')
-    ax1.plot(h3[0],h3[1]-h3[2],label='hanning 3')
-    ax1.plot(h4[0],h4[1]-h4[2],label='hanning 4')
+    ax1.plot(nf[0],nf[1],label='no filter')
+    ax1.plot(h1[0],h1[1],label='hanning 1')
+    ax1.plot(h2[0],h2[1],label='hanning 2')
+    ax1.plot(h3[0],h3[1],label='hanning 3')
+    ax1.plot(h4[0],h4[1],label='hanning 4')
     ax1.set_title(name)
     ax1.vlines(float(rv),-20,20,'r')
     ax1.set_xlabel('Velocity [km/s]')
@@ -38,8 +35,7 @@ def plot_all_spectra(name,nf,h1,h2,h3,h4,rv):
 
 def single_spectrum_plot(name,array,rv):
     fig1,ax1 = plt.subplots()
-    ax1.plot(array[0],array[1]-array[2],label='galaxy')
-    ax1.plot(array[0],array[2],label='background')
+    ax1.plot(array[0],array[1],label='galaxy')
     ax1.vlines(float(rv),-20,20,'r')
     ax1.set_xlabel('Velocity [km/s]')
     ax1.set_ylabel('flux [Jy/BA]')
@@ -54,32 +50,23 @@ def get_w50(name):
     high_x = float(input('high vel val: '))
     mask = (arrays[plot][0]>=low_x) & (arrays[plot][0]<=high_x)
     peak_vels = arrays[plot][0][mask]
-    peak_flux = arrays[plot][1][mask]-arrays[plot][2][mask]
+    peak_flux = arrays[plot][1][mask]
     plt.close()
-    fig1,ax1 = plt.subplots()
-    ax1.plot(peak_vels,peak_flux,label='galaxy')
-    ax1.set_xlabel('Velocity [km/s]')
-    ax1.set_ylabel('flux [Jy/BA]')
-    ax1.legend()
-    ax1.set_title(name+'SECOND?')
-    plt.show(block=False)
 
     peak_index = np.argmax(peak_flux)
     max_val = peak_flux[peak_index]
     hm = max_val/2.0
-    l_index = peak_index
-    while peak_flux[l_index]>hm:
-        l_index-=1
-    r_index = peak_index
-    while peak_flux[r_index]>hm:
-        r_index+=1
+    l_index = 0
+    while peak_flux[l_index] < hm:
+        l_index+=1
+    r_index = len(peak_vels)-1
+    while peak_flux[r_index]<hm:
+        r_index-=1
     fwhm=abs(peak_vels[r_index]-peak_vels[l_index])
     step_size = abs(peak_vels[peak_index]-peak_vels[peak_index-1])
     rad_vel = (peak_vels[r_index]+peak_vels[l_index])/2.0
     print('FWHM: ',fwhm,' +/- ', step_size,' km/s')
     print('RV: ',rad_vel, ' +/- ', step_size*2,' km/s')
-    plt.close()
-
 
     fig2,ax2 = plt.subplots()
     ax2.plot(peak_vels,peak_flux,label='galaxy')
@@ -91,18 +78,33 @@ def get_w50(name):
     plt.show(block=False)
     return fwhm,rad_vel,step_size
 
-def save_csv(row,fwhm,rad_vel,fwhm_uncert,rv_uncert):
-    adds = ['-','-','-']
-    new_row = row +adds
-    new_row[9] =str(rad_vel)
-    new_row[10]=str(rv_uncert)
-    new_row[11]=str(fwhm)
-    new_row[12]=str(fwhm_uncert)
-    with open('/users/jburke/ebhis_scripts/w50_stuff/rv_w50_vals.csv','a') as f:
-        writer = csv.writer(f)
-        writer.writerow(new_row)
+def save_csv(name,rv,uncert1,fwhm,uncert2):
+    gal_results=[]
+    with open('/users/jburke/ebhis_scripts/w50_stuff/new_vals.csv','r') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            gal_results.append(row)
 
 
+    gals_analysed=[]
+    for i in gal_results:
+        gals_analysed.append(i[0])
+        if i[0]==name:
+            i[1]=str(rv)
+            i[2]=str(uncert1)
+            i[3]=str(fwhm)
+            i[4]=str(uncert2)
+    #append gal vals if not already analysed
+    if name not in gals_analysed:
+        new_row = [name,str(rv),str(uncert1),str(fwhm),str(uncert2)]
+        gal_results.append(new_row)
+
+    with open('/users/jburke/ebhis_scripts/w50_stuff/new_vals.csv','w') as f:
+        writer=csv.writer(f)
+        header = ['name','rv','+/-','w50','+/-']
+        writer.writerow(header)
+        writer.writerows(gal_results)
 
 with open('/users/jburke/ebhis_scripts/w50_stuff/ready_to_analyse.csv','r') as f:
     reader = csv.reader(f)
@@ -123,9 +125,9 @@ with open('/users/jburke/ebhis_scripts/w50_stuff/ready_to_analyse.csv','r') as f
             fwhm,rad_vel,step_size=get_w50(name)
             save_vals = input('Save? y/n ')
             if save_vals =='y':
-                save_csv(row,fwhm,rad_vel,step_size,step_size*2)
+                save_csv(name,fwhm,rad_vel,step_size,step_size*2)
         elif vis =='n':
-            save_csv(row,'-','-','-','-')
+            save_csv(name,'-','-','-','-')
         plt.close()
 
         
