26580ac1530910f40a45546e8580ef754e988bbb
JBurke789 <burkej13@tcd.ie>
2023-10-18 15:17:32 +0200

    plots single spectrum to then be cropped and saved

--- w50_stuff/single_spectrum_plot.py
+++ w50_stuff/single_spectrum_plot.py
@@ -4,28 +4,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-arrays = np.load('NGC4449/spectrum.npy')
+array = np.load('man_hanning_spectrum2.npy')
 
 low_x = 100.
 high_x = 400.
-mask = (arrays[0]>=low_x) & (arrays[0]<=high_x)
-peak_vels = arrays[0][mask]
-peak_flux = arrays[1][mask]
+mask = (array[0]>=low_x) & (array[0]<=high_x)
+peak_vels = array[0][mask]
+peak_flux = array[1][mask]
 
 peak_bit = 1.2 * np.max(peak_flux)
 
 
-w50 = 207.5
-rv = 157.16
+w50 = 41.2228790119
+
+rv = 360.801148758
+
 print(peak_bit)
+name ='DDO046'
 
 fig1,ax1 = plt.subplots()
-ax1.plot(arrays[0],arrays[1],label='gal')
+ax1.plot(array[0],array[1],'k',linewidth=0.7)
+ax1.set_title(name)
 ax1.set_xlabel('Velocity [km/s]')
 ax1.set_ylabel('Flux per Beam Area [Jy/BA]')
-#ax1.set_title(name)
-ax1.legend()
-ax1.set_xlim(rv-w50,rv+w50)
-ax1.set_ylim(-peak_bit/5,peak_bit)
-#fig1.savefig(name+'/spectrum.png',dpi=600)
+    #ax1.set_xlim(rv-w50*2.5,rv+w50*2.5)
+    #ax1.set_ylim(np.min(array[1])*1.2,peak_bit)
+ax1.vlines(rv,np.min(array[1]),np.max(array[1]),'r',linewidth=0.7,alpha=0.5,label='v_r')
+closest_index1 = np.argmin(np.abs(array[0] - rv-0.5*w50))
+closest_index2 = np.argmin(np.abs(array[0] - rv+0.5*w50))
+ax1.plot(array[0][closest_index1],array[1][closest_index1],'.',color='r',label='w50',linewidth=0.001,alpha=0.5)
+ax1.plot(array[0][closest_index2],array[1][closest_index2],'.',color='r',label='w50',linewidth=0.001,alpha=0.5)
 plt.show()
