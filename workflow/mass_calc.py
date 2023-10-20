d93f44bcd25738b252afb89e082a5510b4352a37
JBurke789 <burkej13@tcd.ie>
2023-10-20 16:09:51 +0200

    calculates mass for annulus method galaxies

--- workflow/mass_calc.py
+++ workflow/mass_calc.py
@@ -10,11 +10,27 @@ with open('/users/jburke/ebhis_scripts/workflow_results/final_results.csv','r') 
 
 
 def mass_calc(dist,flux,uncert):
-    m= 2.36e5 * (dist**2)* flux
-    u = 2.36* (dist**2)*uncert
+    m= 2.36e5 * (dist**2)* flux *1e-6
+    u = 2.36* (dist**2)*uncert*1e-6
 
     return m , u
 
 
 mass_results=[]
+for row in gal_rows:
+    name = row[0]
+    dist = float(row[3])
+    flux = float(row[7])
+    f_uncert= float(row[8])
 
+    mass,uncert = mass_calc(dist,flux,f_uncert)
+    new_row = [name,flux,f_uncert,mass,uncert]
+    mass_results.append(new_row)
+    print(new_row)
+
+
+with open('/users/jburke/ebhis_scripts/workflow_results/mass_results.csv','w') as f:
+    writer = csv.writer(f)
+    header = ['name','flux [Jy/BA]','+/-','mass [M_sol x10^6]','+/-']
+    writer.writerow(header)
+    writer.writerows(mass_results)
