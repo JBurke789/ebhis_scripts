7129a4f426df017bfc27ec34f09809076c53dffc
JBurke789 <burkej13@tcd.ie>
2023-11-06 18:16:37 +0100

    generates hanning filtered images four times

---
+++ gen_tools/hanning_filtering.py
@@ -0,0 +1,31 @@
+import csv
+
+"""
+runs through csv of galaxies and runs hanning filtering 4 times for each galaxy
+
+run in CASA
+source /vol/software/software/astro/casa/initcasa.sh
+
+
+"""
+
+def run_han(name):
+    ia.open(name+'/raw_image.im') # open the file
+    ia.hanning(name+'/hanning_smoothed1.im')# submit outfile and set (drop pixels) to false
+    ia.close()
+    ia.open(name+'/hanning_smoothed1.im')
+    ia.hanning(name+'/hanning_smoothed2.im')
+    ia.close()
+    ia.open(name+'/hanning_smoothed2.im')
+    ia.hanning(name+'/hanning_smoothed3.im')
+    ia.close()
+    ia.open(name+'/hanning_smoothed3.im')
+    ia.hanning(name+'/hanning_smoothed4.im')
+    ia.close()
+    print("Hanning run on ", name)
+
+with open('/users/jburke/ebhis_scripts/catagorisation/cat_results/MW_overlap.csv','r') as f:
+    reader = csv.reader(f)
+    head = next(reader)
+    for row in reader:
+        run_han(row[0])
