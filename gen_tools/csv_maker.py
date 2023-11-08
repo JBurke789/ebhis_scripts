b9c4ff333956be3e0f11c416dd7f9cea5ebbf9e5
JBurke789 <burkej13@tcd.ie>
2023-11-08 19:16:49 +0100

    .

--- gen_tools/csv_maker.py
+++ gen_tools/csv_maker.py
@@ -4,7 +4,7 @@ import csv
 Makes blank csv file with header
 '''
 
-with open('/users/jburke/ebhis_scripts/w50_stuff/new_vals.csv','w') as f:
+with open('/users/jburke/ebhis_scripts/new_spec/results.csv','w') as f:
     writer = csv.writer(f)
-    header = ['name','rv','+/-','w50','+/-']
+    header = ['name','Flux [Jy km/s BA^-1]','+/-','low vel [km/s]','high vel [km/s]','hanning level']
     writer.writerow(header)
