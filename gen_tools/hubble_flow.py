0441f94d06345663c654ff61480cb89c1ebeb3ad
JBurke789 <burkej13@tcd.ie>
2024-01-30 15:12:09 +0100

    plots rv vs dist for gals and groups. Shows hubble relation for latest H0 vals

--- gen_tools/hubble_flow.py
+++ gen_tools/hubble_flow.py
@@ -7,35 +7,46 @@ import csv
 '''
 Plots rv vs distance to investigate hubble flow
 '''
-rv_vals =[]
-rv_uncert = []
-names =[]
-with open('/users/jburke/ebhis_scripts/w50_stuff/fixed_vals.csv','r') as f:
+rv = []
+dist =[]
+with open('/users/jburke/Desktop/results/full_results.csv','r') as f:
     reader = csv.reader(f)
     header = next(reader)
     for row in reader:
-        if row[1]=='-':
+        if row[9]=='-':
             pass
         else:
-            names.append(row[0])
-            rv_vals.append(float(row[1]))
-            rv_uncert.append(float(row[2]))
+            rv.append(float(row[9]))
+            dist.append(float(row[3]))
 
 
-dist=[]
-for i in names:
-    with open('/users/jburke/ebhis_scripts/workflow_results/final_results.csv','r') as f:
-        reader = csv.reader(f)
-        header = next(reader)
-        for row in reader:
-            if i == row[0]:
-                dist.append(float(row[3]))
 
 
+g_rv =[]
+g_dist =[]
+with open('/users/jburke/Desktop/results/group_latex_full_results.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        g_dist.append(float(row[1]))
+with open('/users/jburke/ebhis_scripts/group_gal_results/rv_w50_results.csv','r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        g_rv.append(float(row[1]))
+
+
+def hubble_line(D,h_0):
+    return h_0*D
+
+dists=np.arange(0,16,1)
 fig, ax = plt.subplots()
 
-ax.scatter(dist,rv_vals,s=0.5)
-ax.errorbar(dist,rv_vals,yerr=rv_uncert,fmt='o')
+ax.scatter(dist,rv,s=3,color='b',label='Isolated',)
+ax.scatter(g_dist,g_rv,s=3,color='r',label='Groups')
+ax.plot(dists,hubble_line(dists,74.6),'k--',linewidth=0.7,alpha=0.7,label='$H_0 = 74.6$')
+ax.plot(dists,hubble_line(dists,67.4),'k-.',linewidth=0.7,alpha=0.7,label='$H_0 =67.4$')
+plt.legend(loc=4)
 ax.set_ylabel('Radial Velocity [km/s]')
 ax.set_xlabel('Distance [Mpc]')
 plt.show()
