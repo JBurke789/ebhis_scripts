9050f37662c2f45fc718bf6f1a5553821ab85f2d
JBurke789 <burkej13@tcd.ie>
2024-01-30 17:50:42 +0100

    streamlined approach to be more efficient for many cubes and chunks WIP

--- ML_stuff/plot_chunk_mom_maps.py
+++ ML_stuff/plot_chunk_mom_maps.py
@@ -13,110 +13,23 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 makes mom map from fits file for single galaxy.
 run in full list directory
 """
-out_files=['chunk1.im','chunk2.im','chunk3.im','chunk4.im','chunk5.im','chunk6.im','chunk7.im','chunk8.im']
-vels = [[-1358.0,0],[0.0,2500],[2500,5000],[5000,7500],[7500,10000],[10000,12500],[12500,15000],[15000,18500]]
+chunks=['chunk0.im','chunk1.im','chunk2.im','chunk3.im']
+vels = [[300,2300],[2300,4300],[4300,6300],[6300,8300]]
   
 
-def read_data(filepath):
+def read_data(cube,chunk,mom_map):
+    filepath = cube+chunk+'_'+mom_map+'.fits'
     hdul= fits.open(filepath)
     array = hdul[0].data
     header = hdul[0].header
     hdul.close()
-    return(array,header)
+    wcs=WCS(header)
+    wcs = wcs.dropaxis(2)
+    return(array,header,wcs)
 
 def make_string(vel):
     string= str(vel[0])+ ' - '+str(vel[1])+ ' km/s '
     return string
-
-def visualize_image(array1,array2,array3,array4,array5,array6,array7,array8,min,max):
-   
-    fig = plt.figure()
-    fig.suptitle('Moment 2 maps')
-    
-    fig.add_subplot(3,3,1)
-    #im1=plt.imshow(array1,vmin=min,vmax=max*0.1)
-    #im1=plt.imshow(array1,vmin=vels[0][0],vmax=vels[0][1])
-    im1=plt.imshow(array1,vmin=0,vmax=1360)
-    plt.title(make_string(vels[0]))
-    plt.xlabel('')
-    plt.ylabel('')
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-    
-
-    fig.add_subplot(332)
-    #im2=plt.imshow(array2,vmin=min,vmax=max)
-    #im2=plt.imshow(array2,vmin=vels[1][0],vmax=vels[1][1])
-    im2=plt.imshow(array2,vmin=0,vmax=2500)
-    plt.title(make_string(vels[1]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(333)
-    #im3=plt.imshow(array3,vmin=min,vmax=max)
-    #im3=plt.imshow(array3,vmin=vels[2][0],vmax=vels[2][1])
-    im3=plt.imshow(array3,vmin=0,vmax=3500)
-    plt.title(make_string(vels[2]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(334)
-    #im4=plt.imshow(array4,vmin=min,vmax=max)
-    #im4=plt.imshow(array4,vmin=vels[3][0],vmax=vels[3][1])
-    im4=plt.imshow(array4,vmin=0,vmax=2500)
-    plt.title(make_string(vels[3]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(335)
-    #im5=plt.imshow(array5,vmin=min,vmax=max)
-    #im5=plt.imshow(array5,vmin=vels[4][0],vmax=vels[4][1])
-    im5=plt.imshow(array5,vmin=0,vmax=2500)
-    plt.title(make_string(vels[4]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(336)
-    #im6=plt.imshow(array6,vmin=min,vmax=max)
-    #im6=plt.imshow(array6,vmin=vels[5][0],vmax=vels[5][1])
-    im6=plt.imshow(array6,vmin=0,vmax=2500)
-    plt.title(make_string(vels[5]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(337)
-    #im7=plt.imshow(array7,vmin=min,vmax=max)
-    #im7=plt.imshow(array7,vmin=vels[6][0],vmax=vels[6][1])
-    im7=plt.imshow(array7,vmin=0,vmax=2500)
-    plt.title(make_string(vels[6]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    fig.add_subplot(338)
-    #im8=plt.imshow(array8,vmin=min,vmax=max)
-    #im8=plt.imshow(array8,vmin=vels[7][0],vmax=vels[7][1])
-    im8=plt.imshow(array8,vmin=0,vmax=3500)
-    plt.title(make_string(vels[7]))
-    cbar=plt.colorbar()
-    cbar.formatter = ScalarFormatter(useMathText=False)
-    cbar.ax.set_ylabel(' km/s')
-    cbar.update_ticks()
-
-    plt.show()
 
 def min_max(arrays):
     maxes=[]
@@ -126,9 +39,148 @@ def min_max(arrays):
         mins.append(np.min(array))
     return max(maxes),min(mins)
 
-def make_mask(array, min,max):
+def vis_mom0(cube,array,wcs):
+    max,min = min_max(array)
+    fig = plt.figure()
+    fig.suptitle(cube+' - Moment 0 Maps')
+    
+    fig.add_subplot(141,projection=wcs)
+    im1=plt.imshow(array[0],vmin=min,vmax=max)
+    plt.title(make_string(vels[0]))
+
+    fig.add_subplot(142,projection=wcs)
+    im2=plt.imshow(array[1],vmin=min,vmax=max)
+    plt.title(make_string(vels[1]))
+
+    fig.add_subplot(143,projection=wcs)
+    im3=plt.imshow(array[2],vmin=min,vmax=max)
+    plt.title(make_string(vels[2]))
+    
+    fig.add_subplot(144,projection=wcs)
+    im4=plt.imshow(array[3],vmin=min,vmax=max)
+    plt.title(make_string(vels[3]))
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    cbar = plt.colorbar(im1,cax=cbar_ax)
+    cbar.set_label('K km/s')
+    plt.show()
+
+def vis_mom1(cube,array,wcs):
+    fig = plt.figure()
+    fig.suptitle(cube+' - Moment 1 Maps')
+    
+    fig.add_subplot(141,projection=wcs)
+    im1=plt.imshow(array[0],vmin=300,vmax=2300)
+    plt.title(make_string(vels[0]))
+    cbar=plt.colorbar()
+    cbar.formatter = ScalarFormatter(useMathText=False)
+    cbar.ax.set_ylabel('km/s')
+    cbar.update_ticks()
+
+    fig.add_subplot(142,projection=wcs)
+    im2=plt.imshow(array[1],vmin=2300,vmax=4300)
+    plt.title(make_string(vels[1]))
+    cbar=plt.colorbar()
+    cbar.formatter = ScalarFormatter(useMathText=False)
+    cbar.ax.set_ylabel('km/s')
+    cbar.update_ticks()
+
+    fig.add_subplot(143,projection=wcs)
+    im3=plt.imshow(array[2],vmin=4300,vmax=6300)
+    plt.title(make_string(vels[2]))
+    cbar=plt.colorbar()
+    cbar.formatter = ScalarFormatter(useMathText=False)
+    cbar.ax.set_ylabel('km/s')
+    cbar.update_ticks()
+
+    fig.add_subplot(144,projection=wcs)
+    im4=plt.imshow(array[3],vmin=6300,vmax=8300)
+    plt.title(make_string(vels[3]))
+    cbar=plt.colorbar()
+    cbar.formatter = ScalarFormatter(useMathText=False)
+    cbar.ax.set_ylabel('km/s')
+    cbar.update_ticks()
+    plt.show()
+
+def vis_mom2(cube,array,wcs):
+    fig = plt.figure()
+    fig.suptitle(cube+' - Moment 2 Maps')
+    
+    fig.add_subplot(141,projection=wcs)
+    im1=plt.imshow(array[0],vmin=0,vmax=2000)
+    plt.title(make_string(vels[0]))
+
+    fig.add_subplot(142,projection=wcs)
+    im2=plt.imshow(array[1],vmin=0,vmax=2000)
+    plt.title(make_string(vels[1]))
+
+    fig.add_subplot(143,projection=wcs)
+    im3=plt.imshow(array[2],vmin=0,vmax=2000)
+    plt.title(make_string(vels[2]))
+
+    fig.add_subplot(144,projection=wcs)
+    im4=plt.imshow(array[3],vmin=0,vmax=2000)
+    plt.title(make_string(vels[3]))
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    cbar = plt.colorbar(im1,cax=cbar_ax)
+    cbar.set_label('km/s')
+    plt.show()
+
+def make_mask(array,min,max):
     masked_array = np.ma.masked_outside(array, min, max)
     return masked_array
+
+def rms(array):
+    mu_sqrd =np.mean(array**2)
+    rms = np.sqrt(mu_sqrd)
+    return rms
+
+def get_stats(array):
+    return [np.mean(array),np.std(array),rms(array)]
+
+#to be done for each cube
+cube = 'cube0'
+arrays_m0=[]#get arrays
+arrays_m1=[]
+arrays_m2=[]
+for i in range(4):
+    array,header,wcs = read_data(cube,chunks[i],'m0')
+    arrays_m0.append(array[0])
+    array,header,wcs = read_data(cube,chunks[i],'m1')
+    arrays_m1.append(array[0])
+    array,header,wcs = read_data(cube,chunks[i],'m2')
+    arrays_m2.append(array[0])    
+vis_mom0(cube,arrays_m0,wcs)#plot arrays
+vis_mom1(cube,arrays_m1,wcs)
+vis_mom2(cube,arrays_m2,wcs)
+
+
+m0_mean=[]
+m0_std=[]
+m0_rms=[]
+for chunk in range(4):
+    stats=get_stats(arrays_m0[chunk])
+    print(stats)
+    #do for other mom maps
+
+'''
+for j in range(5):
+    cube = 'cube'+str(j)
+    arrays=[]
+    for i in range(4):
+        array,header,wcs = read_data(cube,chunks[i],'m0')
+        arrays.append(array[0])
+    vis_mom0(cube,arrays,wcs)
+
+done=input('y:')
+'''
+
+
+
+"""
 
 def rms(array):
     mu_sqrd =np.mean(array**2)
@@ -156,4 +208,4 @@ for i in range(len(out_files)):
 
 max,min=min_max(arrays)
 
-visualize_image(arrays[0],arrays[1],arrays[2],arrays[3],arrays[4],arrays[5],arrays[6],arrays[7],-200,500)
+visualize_image(arrays[0],arrays[1],arrays[2],arrays[3],arrays[4],arrays[5],arrays[6],arrays[7],-200,500)"""
